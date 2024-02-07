9a09b1faf0a0a9f2e29b610c19fc2b69d0fbb800
Nero One <info@nero1.net>
2024-02-07 09:40:42 +0800

    feat: edit home ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { ModeToggle } from "@/components/ui/theme-toggle";
 
 const data = {
-  title: "KISAHARI",
+  title: "LOGBOOK-2078",
   version: "v010",
   entries: [
     {
@@ -31,27 +31,31 @@ const data = {
     {
       date: "03:34 - Tue 5 feb 24",
       heading: "permission",
-      content: `nothing is real, everything is permitted
-`,
+      content: `nothing is real, everything is permitted`,
     },
   ],
   footer: {
-    copy: "© 2078",
+    copy: "KISAHARI © 2078",
+    // copy: "© 2078",
   },
 };
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-between w-full h-full p-8 bg-zinc-100 dark:bg-zinc-900 font-extralight">
+    <main className="flex flex-col min-h-screen items-center justify-between w-full h-full p-8 bg-zinc-100/95 dark:bg-zinc-900/90 font-extralight z-10">
       <div className="flex flex-row justify-between w-full items-center ring-1 ring-zinc-700 px-4 py-2 rounded-t-xl rounded-b-sm">
         <div className="flex-1 flex flex-row gap-2">
           <h1 className="text-xl font-normal">[{data.title}]</h1>
           <span className="self-end text-sm">{data.version}</span>
         </div>
-        <ModeToggle />
+        <div className="uppercase py-2">01:28 - Wed 7 feb 24</div>
+        {/* <ModeToggle /> */}
       </div>
       {/* <div className="flex flex-row justify-between w-full max-w-[95w] items-center py-1 px-4 bg-zinc-800 rounded-full"></div> */}
-      <div className="flex-1 relative h-full flex flex-col gap-4 p-16 items-start w-full ring-1 ring-zinc-700 overflow-y-auto max-h-[82vh] rounded-sm">
+      <div
+        className="flex-1 relative h-full flex flex-col gap-4 p-8 sm:p-16 items-start w-full ring-1 ring-zinc-700
+      overflow-y-auto max-h-[82vh] rounded-sm scrollbar-thin scrollbar-thumb-zinc-800 scrollbar-track-zinc-900"
+      >
         <div className="flex flex-row items-center gap-2 group">
           <div className="p-3 scale-50 ring-2 ring-lime-500 group-hover:bg-lime-500 group-hover:animate-pulse rounded-full" />
           <div className="uppercase text-zinc-500 group-hover:text-zinc-300">
@@ -66,10 +70,10 @@ export default function Home() {
           <h1 className="uppercase text-xl -ml-1 font-light">
             {data.entries[0].heading}
           </h1>
-          <span>hello, there has been a change in the matrix.</span>
+          <span>Hello, there has been a change in the matrix.</span>
           <p>{data.entries[0].content}</p>
           <p className="max-w-md w-full">
-            <img src="https://source.unsplash.com/random/700x700" alt="" />
+            <img src="https://random.cc0-lib.wtf/api/image" alt="" />
           </p>
           <p className="max-w-prose">{data.entries[0].content}</p>
           <Spacer />
@@ -114,10 +118,18 @@ export default function Home() {
           <Spacer />
         </div>
       </div>
-      <div className="flex flex-row self-start w-[23vw] max-w-[95vw] px-4 py-1 bg-lime-400 rounded-sm"></div>
+      <div className="flex flex-row self-start w-[10vw] max-w-[95vw] px-4 py-1 bg-lime-400 rounded-sm"></div>
       <div className="flex flex-row justify-between w-full items-center ring-1 ring-zinc-700 px-4 py-2 rounded-b-xl rounded-t-sm">
-        <div className="flex-1">+ ADD ENTRY</div>
-        <div>{data.footer.copy}</div>
+        <form
+          action={async () => {
+            "use server";
+            console.log("clicked");
+          }}
+          className="flex-1 max-w-max"
+        >
+          <button>+ ADD ENTRY</button>
+        </form>
+        <div className="text-xs">{data.footer.copy}</div>
       </div>
     </main>
   );
