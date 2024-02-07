44ae918fda2f0d8a7a57077d71df1d578039d4e5
Nero One <info@nero1.net>
2024-02-07 09:40:09 +0800

    feat: add scrollbar, grid bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,10 +32,12 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          <div className="flex flex-col font-mono w-full h-screen">
+          <div className="flex flex-col font-mono w-full h-screen selection:bg-lime-500 selection:text-zinc-800">
             {/* <Sidebar /> */}
+
             <main className="">
               {/* <Navbar /> */}
+              <Background />
               {children}
             </main>
           </div>
@@ -45,3 +47,13 @@ export default function RootLayout({
     </html>
   );
 }
+
+const Background = () => {
+  return (
+    <>
+      {/* <div className="absolute inset-0 -z-10 h-full w-full bg-[linear-gradient(to_right,#777_1px,transparent_1px),linear-gradient(to_bottom,#777_1px,transparent_1px)] bg-[size:60px_60px] pointer-events-none"></div> */}
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(200%_125%_at_50%_10%,#000_40%,#7d15_55%)] pointer-events-none" />
+      <div className="absolute inset-0 h-full w-full bg-transparent bg-[radial-gradient(#ddd_1px,transparent_1px)] [background-size:16px_16px] pointer-events-none -z-10" />
+    </>
+  );
+};
