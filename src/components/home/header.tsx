3576020af8acd3f42490e64d1526e021936fa47f
Nero One <info@nero1.net>
2024-02-10 15:25:17 +0800

    feat: button to hide/show key

--- src/components/home/header.tsx
+++ src/components/home/header.tsx
@@ -11,6 +11,7 @@ import {
 import CurrentTime from "@/components/current-time";
 import { useState } from "react";
 import { useLocalStorage, useReadLocalStorage } from "usehooks-ts";
+import { EyeIcon, EyeOffIcon } from "lucide-react";
 
 const Header = ({
   data,
@@ -23,9 +24,7 @@ const Header = ({
   setModel: (model: string) => void;
   model: string;
 }) => {
-  // const [modelType, setModelType] = useState<"ollama" | "openAI">("openAI");
   const currKey = useReadLocalStorage<string>("openAIKey");
-
   const [modelType, setModelType] = useLocalStorage<"ollama" | "openAI">(
     "modelType",
     "openAI"
@@ -34,6 +33,7 @@ const Header = ({
     "openAIKey",
     currKey!
   );
+  const [showKey, setShowKey] = useState(false);
 
   return (
     <div className="flex flex-row justify-between w-full items-center ring-1 ring-zinc-700 px-6 py-2 rounded-t-xl rounded-b-sm">
@@ -41,7 +41,7 @@ const Header = ({
         <h1 className="text-xl font-normal">[{data.title}]</h1>
         <span className="self-end text-sm">{data.version}</span>
       </div>
-      <div className="flex flex-row items-center gap-4 max-w-lg w-full self-end">
+      <div className="flex flex-row items-center gap-4 max-w-fit w-full self-end">
         {modelType === "ollama" ? (
           <Select
             onValueChange={(value) => {
@@ -64,14 +64,37 @@ const Header = ({
               ))}
             </SelectContent>
           </Select>
+        ) : showKey ? (
+          <>
+            <button
+              onClick={() => setShowKey(false)}
+              className="flex h-10 items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 ring-offset-background focus:outline-none focus:ring-1 focus:ring-lime-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 text-xs"
+            >
+              <EyeOffIcon className="h-5 w-5" />
+            </button>
+
+            <input
+              type="text"
+              className="flex h-10 w-full min-w-fit items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-lime-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 text-xs"
+              placeholder="OPENAI API KEY"
+              value={openAIKey}
+              onChange={(e) => setOpenAIKey(e.target.value)}
+            />
+          </>
         ) : (
-          <input
-            type="text"
-            className="flex h-10 w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-lime-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 text-xs"
-            placeholder="OPENAI API KEY"
-            value={openAIKey}
-            onChange={(e) => setOpenAIKey(e.target.value)}
-          />
+          <>
+            <button
+              onClick={() => setShowKey(true)}
+              className="flex h-10 items-center justify-between rounded-md border border-input bg-transparent px-3 py-2  ring-offset-background focus:outline-none focus:ring-1 focus:ring-lime-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 text-xs"
+            >
+              <EyeIcon className="h-5 w-5" />
+            </button>
+            <input
+              type="text"
+              className="flex h-10 min-w-fit w-full items-center justify-between rounded-md border border-input bg-transparent px-3 py-2 ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:ring-1 focus:ring-lime-500 focus:ring-offset-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 text-xs"
+              value={openAIKey && openAIKey.replace(/./g, "*")}
+            />
+          </>
         )}
 
         <Select
