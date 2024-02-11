1b1dec93d96bac9017401c85507b4abe10949a46
Nero One <info@nero1.net>
2024-02-11 20:26:34 +0800

    chore: ui improvement

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,16 +2,12 @@ import Container from "@/components/home/container";
 
 export default function Loading() {
   return (
-    <Container>
-      <div></div>
-
+    <Container className="justify-center">
       <img
         src="/icon.svg"
         alt="loading"
         className="w-[vw20] h-[vw20] animate-pulse"
       />
-
-      <div></div>
     </Container>
   );
 }
--- src/components/home/container.tsx
+++ src/components/home/container.tsx
@@ -1,6 +1,19 @@
-const Container = ({ children }: { children: React.ReactNode }) => {
+import { cn } from "@/lib/utils";
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) => {
   return (
-    <main className="flex relative flex-col min-h-screen items-center justify-between w-full h-full p-8 bg-zinc-100/15 dark:bg-zinc-900/10 font-extralight z-10 max-h-screen">
+    <main
+      className={cn(
+        "flex relative flex-col min-h-screen items-center justify-between w-full h-full p-8 bg-zinc-100/15 dark:bg-zinc-900/10 font-extralight z-10 max-h-screen",
+        className
+      )}
+    >
       {children}
     </main>
   );
